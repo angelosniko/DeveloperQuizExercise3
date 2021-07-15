bdfe961ae500f8fa20e448a549c843048aa62170
nikolopoulosangelos1 <nikolopoulosangelos1@gmail.com>
2021-07-15 13:57:20 +0300

    The program is about Tip splitter calculator

---
+++ src/TipCalculator/TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tip_Splitter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void MyButton_Clicked(object sender, EventArgs e)
+        {
+            var price = int.Parse(NameTextBox.Text);
+
+            Application.Current.Properties["Name"] = NameTextBox.Text;
+            Navigation.PushAsync(new Page1(price));
+
+        }
+    }
+}
---
+++ src/TipCalculator/TipCalculator/TipCalculator/Page1.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tip_Splitter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public int value;
+
+        public Page1(int value)
+        {
+            InitializeComponent();
+            this.value = value;
+           
+           
+        }
+
+        private void PercentSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Slider_Label.Text = PercentSlider.Value.ToString()+"%";
+            
+            calculate();
+            picker_SelectedIndexChanged(sender, e);
+        
+
+
+
+
+        }
+
+
+        private void calculate() {
+            
+            var tipprice = (PercentSlider.Value * value) / 100;
+           
+            TipLabel.Text = String.Format("{0:0.00}", tipprice.ToString());
+
+        }
+
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var get = picker.Items[picker.SelectedIndex];
+                var pp = Convert.ToDouble(get);
+                var tipprice = (PercentSlider.Value * value) / 100;
+                var PricePerPersonValue = Math.Round((value + tipprice) / pp,2);
+                PricePerPerson.Text =  String.Format("{0:0.00}", PricePerPersonValue.ToString());
+                
+                calculate();
+
+
+                
+
+            }
+            catch {  DisplayAlert("Alert","Please select number of people","OK"); }
+       
+        }
+
+        private void TipLabel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(TipLabel.Text))
+            {
+                Slider_Label.Text = "0";
+                PercentSlider.Value = 0;
+
+
+
+
+
+            }
+            else {
+                var parse = double.Parse(TipLabel.Text);
+                var valParse = (parse / value) * 100;
+                
+                Slider_Label.Text = String.Format("{0:0.00}", valParse.ToString())+"%";
+                PercentSlider.Value = valParse;
+
+            }
+           
+
+        }
+    }
+}
