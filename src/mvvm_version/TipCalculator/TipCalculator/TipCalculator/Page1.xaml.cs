7d607977ed01567ec10f679aa794ba46cb4c8ee5
nikolopoulosangelos1 <nikolopoulosangelos1@gmail.com>
2021-07-15 14:59:32 +0300

    MVVM version added

---
+++ src/mvvm_version/TipCalculator/TipCalculator/TipCalculator/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalculator;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tip_Splitter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public int value;
+
+        public Page1(int value)
+        {
+            InitializeComponent();
+            this.value = value;
+      
+            var vm = new Page1ViewModel(value);
+            BindingContext = vm;
+
+            
+
+
+        }
+        
+
+
+   
+    }
+}
---
+++ src/mvvm_version/TipCalculator/TipCalculator/TipCalculator/Page1ViewModel.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TipCalculator
+{
+   public class Page1ViewModel : ViewModelBase
+    {
+
+        private double _constvalue;
+        private double _slidervalue;
+        public List<KeyValuePair> NumberList { get; set; }
+
+        public List<KeyValuePair> GetNumbers()
+        {
+
+            var keyvalue = new List<KeyValuePair>() {
+            new KeyValuePair(){key=1,Value="1" },
+            new KeyValuePair(){key=2,Value="2" },
+            new KeyValuePair(){key=3,Value="3" },
+            new KeyValuePair(){key=4,Value="4" },
+            new KeyValuePair(){key=5,Value="5" },
+            new KeyValuePair(){key=6,Value="6" },
+             new KeyValuePair(){key=7,Value="7" },
+              new KeyValuePair(){key=8,Value="8" },
+               new KeyValuePair(){key=9,Value="9" },
+                new KeyValuePair(){key=10,Value="10" }
+
+
+
+            };
+
+            return keyvalue;
+
+
+        }
+
+
+
+        public class KeyValuePair
+        {
+
+            public int key { get; set; }
+            public String Value { get; set; }
+
+        }
+
+        public Page1ViewModel(double constvalue) {
+
+            _constvalue = constvalue;
+            NumberList = GetNumbers().OrderBy(a => a.key).ToList();
+
+        }
+
+        public string Text
+        {
+            get { return (_constvalue * _slidervalue / 100).ToString(); }
+            set
+            {
+                var p = double.Parse(value);
+                SliderValue = p / _constvalue * 100;
+                
+                
+
+
+
+
+            }
+        }
+
+        private KeyValuePair _selectItem;
+        public KeyValuePair SelectectItem {
+
+            get { return _selectItem; }
+
+            set {
+                if (_selectItem != value) {
+
+                    _selectItem = value;
+                    OnPropertyChanged(nameof(SelectectItem));
+                    OnPropertyChanged(nameof(PriceValue));
+
+                }
+
+            
+            
+            }
+
+            
+
+        
+        
+        
+        }
+
+
+        public double PriceValue {
+
+
+            get {
+                var val = _constvalue * _slidervalue / 100;
+                if (_selectItem != null)
+                {
+
+                    var val2 = (val + _constvalue) / Convert.ToDouble(_selectItem.Value);
+                    return val2;
+                }
+                else {
+
+
+                    return 0;
+                }
+               
+
+                
+
+            }
+
+
+            private set { 
+            
+            
+            
+            }
+        }
+
+
+
+
+
+        public double SliderValue
+        {
+            get { return _slidervalue; }
+            set
+            {
+                _slidervalue = value;
+
+                OnPropertyChanged(nameof(SliderValue));
+                OnPropertyChanged(nameof(Text));
+                OnPropertyChanged(nameof(PriceValue));
+
+            }
+        }
+    }
+}
